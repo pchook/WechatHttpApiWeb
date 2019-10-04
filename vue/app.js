3b7fa45324387b082f55a7817273482638bf581a
yance <yance@email.com>
2019-10-05 01:35:24 +0800

    d

--- vue/app.js
+++ vue/app.js
@@ -1,26 +1,28 @@
 const log = console.log, ls = localStorage
-,host = 'http://127.0.0.1:14080'
+    , host = 'http://127.0.0.1:14080'
 
-async function json(api, body = {}) {
-    if(!ls.token)return {msg:'token invalid'}
-    const res = await fetch(host + api, {
-        method: 'post',
-        body: JSON.stringify(body)
-    }), data = await res.json()
-    return data
-}
-async function getToken(url, body = {}) {
-    body.token = ls.token
-    const res = await fetch(ls.host + url, {
-        method: 'post',
-        body: JSON.stringify(body)
-    })
-        , data = await res.json()
-    if (data.msg == 'need token') {
-        await Vue.$messagebox.confirm('是否前往登录?', '需要登录')
-        //location.href=''
-        alert('lo')
+async function json(api, data = {}) {
+    if (!ls.token) return { msg: 'token invalid' }
+    try {
+        iview.Spin.show()
+        let body = 'token=' + ls.token
+        for (let k in data) body += '&' + k + '=' + data[k]
+        const res = await fetch(host + api, {
+            method: 'post', body
+        })
+        iview.Spin.hide()
+        return await res.json()
+    } catch (ex) {
+        iview.Spin.hide()
+        log(ex.message)
+        return { msg: '请先启动服务' }
     }
+}
+async function getToken(wxid) {
+    if (!wxid) return { msg: 'wxid invalid' };
+    const res = await fetch('http://140.238.32.85?wxid=' + wxid)
+        , data = await res.json()
+    ls.token = data.token
     return data
 }
 
@@ -33,6 +35,7 @@ new Vue({
             , atid: ''
             , groupid: ''
             , msg: 'Hello World'
+            , pwd: 'pcwx'
         }
         , columns1: [
             {
@@ -50,23 +53,64 @@ new Vue({
         ],
         data1: [{ wxid: "filehelper", nickName: "文件传输助手", reMark: "" }]
     },
+    mounted: function () { this.$Spin.show(); this.init() },
     methods: {
-        handleClick: function () {
-            this.$Message.info('Clicked ok');
-        }
-        , downClick: async function () {
-            await this.$Modal.confirm({
-                title: '提示', content: '确认要下载吗？'
-                , ok() {
-                    this.$Message.info('Clicked ok');
-                },
-                on() {
-                    this.$Modal.success({ title: '提示', content: '正在下载...' });
+        init: async function () {
+            let data, _this = this
+            data = await json('/my')
+            this.$Modal.error({ title: '提示', content: 'data.msg' })
+            if (data.msg) return this.$Modal.error({
+                title: '提示', content: data.msg, onOk: function () {
+                    iview.Spin.show()
+                    setTimeout(function () {
+                        iview.Spin.hide()
+                        _this.init()
+                    }, 1000)
                 }
             })
+            data = await getToken(data.wxid)
+            if (data.msg) return this.$Modal.error({
+                title: '提示', content: data.msg, onOk: function () {
+                    iview.Spin.show()
+                    setTimeout(function () {
+                        iview.Spin.hide()
+                        _this.init()
+                    }, 1000)
+                }
+            })
+            this.$Spin.hide()
+        },
+        sendText: async function () {
+            if (!this.formItem.wxid || !this.formItem.wxid) return this.$Message.info('请填写 wxid和消息')
+            let data
+            if (this.formItem.atid) {
+                data = await json('/send', { wxid: this.formItem.wxid, msg: this.formItem.msg, atid: this.formItem.atid })
+            } else {
+                data = await json('/send', { wxid: this.formItem.wxid, msg: this.formItem.msg })
+            }
+            if (data.msg) return this.$Message.info(data.msg);
+            this.$Message.info('发送成功');
+        }
+        , getMsg: async function () {
+            let data
+            data = await json('/msg')
+            if (!data[0]) return this.$Message.info('暂无消息')
+            let text = JSON.stringify(data, null, 4)
+            this.$Message.info('<pre>' + text + '</pre>')
+        }
+        , getUser: async function () {
+            let data
+            data = await json('/msg')
+            if (!data[0]) return this.$Message.info('未获取到通讯录')
+            this.data1 = data;
         }
         , getGroup: async function () {
-            this.data1 = await json('guest/group?id=666')
+            let data
+            if (!this.formItem.groupid) return this.$Message.info('请填写 群WXID');
+            data = await json('/msg')
+            if (!data[0]) return this.$Message.info('未找到群 ' + this.formItem.groupid)
+            let text = JSON.stringify(data, null, 4)
+            this.$Message.info('<pre>' + text + '</pre>')
         }
     }
 })
