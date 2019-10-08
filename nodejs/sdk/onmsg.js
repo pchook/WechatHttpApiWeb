24272cc8ac411bdc3a692c925586dd1c871d34ad
yance <yance@email.com>
2019-10-09 07:47:34 +0800

    nodesdk更新

--- nodejs/sdk/onmsg.js
+++ nodejs/sdk/onmsg.js
@@ -2,7 +2,8 @@ const log = console.log
 function onmsg(datas) {
     //对消息数组进行处理
     for (let o of datas) {
-        log(`收到(${o.wxid})的消息`, o.msg)
+        if (o.type != 1) continue//不是文本消息直接跳过
+        log(`收到(${o.wxid})的文本消息`, o.msg)
     }
 
 }
