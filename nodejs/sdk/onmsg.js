2dceae5e815fc0f38382baa834a92e557ffc95d8
yance <yance@email.com>
2019-10-09 07:58:56 +0800

    更新nodejs sdk

--- nodejs/sdk/onmsg.js
+++ nodejs/sdk/onmsg.js
@@ -1,11 +1,22 @@
 const log = console.log
+    , fs = require('fs')
 function onmsg(datas) {
     //对消息数组进行处理
     for (let o of datas) {
         if (o.type != 1) continue//不是文本消息直接跳过
         log(`收到(${o.wxid})的文本消息`, o.msg)
+        write(`[${o.wxid}][${o.sender}]:${o.msg}`)
     }
-
 }
-
+//消息写到文件里看看
+function write(msg) {
+    fs.appendFile(`msg.${time()}.txt`, msg + '\r\n\r\n', function () { });
+}
+function time() {
+    var now = new Date();
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    return `${y}${m < 10 ? "0" + m : m}${d < 10 ? "0" + d : d}`
+}
 module.exports = onmsg
